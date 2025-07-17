ae9b07ff83f8804616e6b9798d3dc89d17f0ea74
Wojciech Piątkowski <chmurson@gmail.com>
2025-07-17 20:38:10 +0200

    refactor: remove unsused code

--- fe/src/services/web-rtc/index.ts
+++
@@ -1,4 +0,0 @@
-export { LeaderService } from "./LeaderService";
-export { ParticipantService } from "./ParticipantService";
-export { SignalingService } from "./SignalingService";
-export * from "./types";
