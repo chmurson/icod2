a39205c65be407a8e8ac153720bef03e6e7a9c95
Wojciech Piątkowski <chmurson@gmail.com>
2025-07-21 18:06:12 +0200

    fix: a bit broken mobile looks of layout (#46)

--- fe/src/components/MainLayout.tsx
+++ fe/src/components/MainLayout.tsx
@@ -13,9 +13,11 @@ export const MainLayout: FC<PropsWithChildren> = ({ children }) => {
 
 const ContentCard: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <Card className="max-w-5xl mx-auto px-4 -mt-20 w-full mb-18 min-h-96">
-      <div className="p-6 px-24 max-md:px-8">{children}</div>
-    </Card>
+    <div className="mx-auto max-w-5xl">
+      <Card className="mx-4 px-4 -mt-20 mb-18 min-h-96 box-border max-sm:px-0">
+        <div className="p-6 px-24 max-md:px-8">{children}</div>
+      </Card>
+    </div>
   );
 };
 
@@ -28,10 +30,10 @@ const HeaderImage: FC = () => {
       className="
 				  b-image
           w-full
-           h-64
-           shadow
-           bg-center
-           bg-[length:auto_256px]
+          h-64
+          shadow
+          bg-center
+          bg-[length:auto_256px]
         "
     />
   );
