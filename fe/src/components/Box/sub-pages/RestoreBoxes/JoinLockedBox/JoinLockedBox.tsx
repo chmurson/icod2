8399d73d0ed06d46792b77393aa28065db9e7a26
Wojciech Piątkowski <chmurson@gmail.com>
2025-07-22 15:36:17 +0200

    feat: share access dropdown for follower as well

--- fe/src/components/Box/sub-pages/RestoreBoxes/JoinLockedBox/JoinLockedBox.tsx
+++ fe/src/components/Box/sub-pages/RestoreBoxes/JoinLockedBox/JoinLockedBox.tsx
@@ -1,6 +1,7 @@
 import { FieldArea } from "@/components/Box/components/FieldArea";
 import { ParticipantItem } from "@/components/Box/components/ParticipantItem";
 import { ShareAccessButton } from "@/components/Box/components/ShareAccessButton";
+import { ShareAccessDropdown } from "@/components/Box/components/ShareAccessDropdown";
 import { useJoinLockedBoxStore } from "@/stores/boxStore/joinLockedBoxStore";
 import { Button } from "@/ui/Button";
 import { Text } from "@/ui/Typography";
@@ -22,6 +23,9 @@ export const JoinLockedBox: React.FC = () => {
   const shareAccessKeyByKeyHolderId = useJoinLockedBoxStore(
     (state) => state.shareAccessKeyByKeyHolderId,
   );
+  const idsOfKeyHoldersToShareWith = Object.entries(shareAccessKeyByKeyHolderId)
+    .filter(([_, value]) => value === true)
+    .map(([key]) => key);
 
   // Only show UI when in connecting/connected/opened state
   if (!["connecting", "connected", "opened"].includes(state)) {
@@ -44,7 +48,22 @@ export const JoinLockedBox: React.FC = () => {
       </Text>
       <div className="flex flex-col gap-4">
         <FieldArea label="Your access key">
-          <ParticipantItem name={you.name} userAgent={you.userAgent} />
+          <ParticipantItem
+            name={you.name}
+            userAgent={you.userAgent}
+            buttonSlot={
+              <ShareAccessDropdown
+                value={idsOfKeyHoldersToShareWith}
+                onChange={actions.toggleSharesAccessKeys}
+                options={onlineKeyHolders.map((kh) => ({
+                  id: kh.id,
+                  name: kh.name,
+                  userAgent: kh.userAgent,
+                  avatar: undefined,
+                }))}
+              />
+            }
+          />
         </FieldArea>
         {onlineKeyHolders.length !== 0 && (
           <FieldArea label="Online users">
