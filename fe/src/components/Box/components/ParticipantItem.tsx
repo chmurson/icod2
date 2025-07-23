07e83b6f8390e9852ffd891cc0e5ae9cf9f9efc0
Wojciech Piątkowski <chmurson@gmail.com>
2025-07-23 19:44:36 +0200

    feat: redisgn keyholder list item - step 1

--- fe/src/components/Box/components/ParticipantItem.tsx
+++ fe/src/components/Box/components/ParticipantItem.tsx
@@ -1,4 +1,4 @@
-import { PersonIcon } from "@radix-ui/react-icons";
+import { Avatar } from "@radix-ui/themes";
 import type { ReactNode } from "react";
 import { Text } from "@/ui/Typography";
 import { UserAgent } from "./UserAgent";
@@ -15,13 +15,17 @@ export const ParticipantItem = ({
   buttonSlot?: ReactNode;
 }) => {
   return (
-    <div className="flex gap-4 items-center justify-between">
-      <div className="flex gap-4 items-center">
-        <PersonIcon width={32} height={32} />
-        <div className="flex flex-col">
+    <div className="flex gap-4 items-center justify-between py-2 border-b border-gray-200">
+      <div className="flex gap-3 items-center">
+        <Avatar
+          fallback={name[0]}
+          radius="full"
+          style={{ height: 48, width: 48 }}
+        />
+        <div className="flex flex-col py-1">
           <div className="flex items-center gap-2">
             {name.trim() && (
-              <Text variant="primaryText">
+              <Text variant="primaryText" className="font-semibold">
                 {name.trim() !== "" ? name : ""}
               </Text>
             )}
