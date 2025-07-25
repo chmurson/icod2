5056916cbedb894bc02899c069efac2c5c596251
michal-kuzma <michal.quzma@gmail.com>
2025-07-25 16:08:36 +0200

    fix: fix

--- fe/src/components/Box/sub-pages/RestoreBoxes/commons/components/ReadyToUnlockLabel.tsx
+++ fe/src/components/Box/sub-pages/RestoreBoxes/commons/components/ReadyToUnlockLabel.tsx
@@ -1,6 +1,5 @@
 import { Badge } from "@radix-ui/themes";
 import type { FC } from "react";
-import { Text } from "@/ui/Typography";
 
 export const ReadyToUnlockLabel: FC = () => {
   return (
@@ -9,10 +8,3 @@ export const ReadyToUnlockLabel: FC = () => {
     </Badge>
   );
 };
-
-<div
-  className="px-2 py-1 rounded-md"
-  style={{ backgroundColor: "var(--accent-9)" }}
->
-  <Text variant="secondaryText" className="text-sm" />
-</div>;
