11985053d4e432b8a8378a205fe2739ee0779c2b
Wojciech Piątkowski <chmurson@gmail.com>
2025-07-27 11:27:54 +0200

    fix: web socket protocol connection on prod

--- fe/src/services/websocket/createWebsocketConnection.tsx
+++ fe/src/services/websocket/createWebsocketConnection.tsx
@@ -2,7 +2,9 @@ import { WebsocketJSONHandler } from "./WebSocketJSONHandler";
 
 const port = import.meta.env.VITE_SIGNALING_PORT;
 const hosname = import.meta.env.VITE_SIGNALING_HOSTNAME;
-const url = `ws://${hosname}:${port}`;
+const isSecure = window.location.protocol === "https:";
+const protocol = isSecure ? "wss" : "ws";
+const url = `${protocol}://${hosname}:${port}`;
 
 export function createWebsocketConnection({
   enableLogging = false,
