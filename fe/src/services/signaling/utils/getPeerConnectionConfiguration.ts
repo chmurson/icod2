a127f5aa60190092c7ed23d5bf8881ab105ca64e
Wojciech Piątkowski <chmurson@gmail.com>
2025-07-30 19:09:00 +0200

    fix: use metered.cd as stun and turn servers

--- fe/src/services/signaling/utils/getPeerConnectionConfiguration.ts
+++ fe/src/services/signaling/utils/getPeerConnectionConfiguration.ts
@@ -1,11 +1,28 @@
 export function getPeerConnectionConfiguration() {
   return {
     iceServers: [
-      { urls: "stun:stun.l.google.com:19302" },
       {
-        urls: ["turn:relay1.expressturn.com:3480"],
-        username: "000000002069235043",
-        credential: "JfpJZDnXwAcMM/1MWleRFh32PNo=",
+        urls: "stun:stun.relay.metered.ca:80",
+      },
+      {
+        urls: "turn:global.relay.metered.ca:80",
+        username: "5473994f7ccf9325b581d163",
+        credential: "c2yQiiEkiUea8ExJ",
+      },
+      {
+        urls: "turn:global.relay.metered.ca:80?transport=tcp",
+        username: "5473994f7ccf9325b581d163",
+        credential: "c2yQiiEkiUea8ExJ",
+      },
+      {
+        urls: "turn:global.relay.metered.ca:443",
+        username: "5473994f7ccf9325b581d163",
+        credential: "c2yQiiEkiUea8ExJ",
+      },
+      {
+        urls: "turns:global.relay.metered.ca:443?transport=tcp",
+        username: "5473994f7ccf9325b581d163",
+        credential: "c2yQiiEkiUea8ExJ",
       },
     ],
     iceTransportPolicy: "all",
